46d6d4a9b73d7a080ec3502d1f57c32aec9d52d5
MacMini <celia.chang@kingone-design.com>
2024-03-29 17:50:36 +0800

    init

---
+++ pages/admin/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+ClientOnly
+  NuxtLayout
+    .testt 
+      h1 1123
+</template>
+  
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import { useCounterStore } from "../../store/index"
+const store = useCounterStore()
+
+const email = ref('');
+const password = ref('');
+
+</script>
+
+<style lang="sass" scoped>
+.test
+  color: red
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+.testt 
+  h1 1123
+</template>
+
+<script setup lang="ts">
+import {useCounterStore} from "../store/index"
+const store = useCounterStore()
+
+</script>
+
+<style lang="sass" scoped>
+.test
+  color: red
+</style>
