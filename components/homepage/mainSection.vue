734e0089aa8c838ad40fbcbce9660ffb8aed6819
Celia Chang <tina8899530@gmail.com>
2024-06-21 18:00:23 +0800

    articles status fix

--- components/homepage/mainSection.vue
+++ components/homepage/mainSection.vue
@@ -16,57 +16,65 @@ section.main__wrapper
 <script lang="ts" setup>
 // import type { Database } from "~/types/supabase";
 interface Articles {
-  id: string;
-  key: string;
-  title: string;
-  publish_date: string;
-  description: string;
+  id: string
+  key: string
+  title: string
+  publish_date: string
+  description: string
+  status: number
+  content: string
+  category_id: number
+  count: number
 }
+
 const props = defineProps<{
-  articles: Articles[];
-  pending: boolean;
-  error: Error | null;
-}>();
-const articlesByYear = ref<{ year: string; articles: unknown }[]>([]);
+  articles: Articles[]
+  pending: boolean
+  error: Error | null
+}>()
+const articlesByYear = ref<{ year: string; articles: unknown }[]>([])
 
 watchEffect(() => {
   if (props.articles) {
-    const groupedByYear = props.articles.reduce(
+    const filteredArticles = props.articles.filter(
+      (article) => article.status !== 0
+    )
+    const groupedByYear = filteredArticles.reduce(
       (acc: { [x: string]: any[] }, article: { publish_date: string }) => {
-        const year = article.publish_date.split("-")[0];
+        const year = article.publish_date.split('-')[0]
         if (!acc[year]) {
-          acc[year] = [];
+          acc[year] = []
         }
-        acc[year].push(article);
-        return acc;
+        acc[year].push(article)
+        return acc
       },
       {}
-    );
+    )
     const articlesArray = Object.entries(groupedByYear).map(
       ([year, articles]) => {
         articles.sort(
           (a: { publish_date: string }, b: { publish_date: string }) =>
             new Date(b.publish_date).getTime() -
             new Date(a.publish_date).getTime()
-        );
-        return { year: year, articles: articles };
+        )
+        return { year: year, articles: articles }
       }
-    );
-    articlesArray.sort((a, b) => Number(b.year) - Number(a.year));
-    articlesByYear.value = articlesArray;
+    )
+    articlesArray.sort((a, b) => Number(b.year) - Number(a.year))
+    articlesByYear.value = articlesArray
   }
-});
+})
 
 // @ts-ignore
 const goToArticle = (key: string) => {
-  console.log(key);
-  window.location.href = `/article/${key}`;
-};
+  console.log(key)
+  window.location.href = `/article/${key}`
+}
 // @ts-ignore
 const dateFormat = (originalDate: string | number | Date) => {
-  const date = new Date(originalDate);
-  return date.toLocaleDateString("en-US", { month: "short", day: "numeric" });
-};
+  const date = new Date(originalDate)
+  return date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })
+}
 </script>
 
 <style lang="sass" scoped>
